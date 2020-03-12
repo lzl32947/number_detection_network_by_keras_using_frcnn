845009e5a58c3f563ad2c1a8d266e890c0d60aef
lzl32947@gmail.com <githublzl980223>
2020-03-12 14:24:19 +0800

    Add classifier network.

---
+++ network/classifier/resnet50_classifier.py
@@ -0,0 +1,99 @@
+from keras.layers import *
+from keras.models import Model
+from keras.utils import plot_model
+
+from layers.ROIPoolingConv import RoiPoolingConv
+
+
+def identity_block_td(input_tensor, kernel_size, filters, stage, block, trainable=True):
+    nb_filter1, nb_filter2, nb_filter3 = filters
+
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = TimeDistributed(Conv2D(nb_filter1, (1, 1), trainable=trainable, kernel_initializer='normal'),
+                        name=conv_name_base + '2a')(input_tensor)
+    x = TimeDistributed(BatchNormalization(axis=3), name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    x = TimeDistributed(Conv2D(nb_filter2, (kernel_size, kernel_size), trainable=trainable, kernel_initializer='normal',
+                               padding='same'), name=conv_name_base + '2b')(x)
+    x = TimeDistributed(BatchNormalization(axis=3), name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = TimeDistributed(Conv2D(nb_filter3, (1, 1), trainable=trainable, kernel_initializer='normal'),
+                        name=conv_name_base + '2c')(x)
+    x = TimeDistributed(BatchNormalization(axis=3), name=bn_name_base + '2c')(x)
+
+    x = Add()([x, input_tensor])
+    x = Activation('relu')(x)
+
+    return x
+
+
+def conv_block_td(input_tensor, kernel_size, filters, stage, block, input_shape, strides=(2, 2), trainable=True):
+    nb_filter1, nb_filter2, nb_filter3 = filters
+
+    conv_name_base = 'res' + str(stage) + block + '_branch'
+    bn_name_base = 'bn' + str(stage) + block + '_branch'
+
+    x = TimeDistributed(Conv2D(nb_filter1, (1, 1), strides=strides, trainable=trainable, kernel_initializer='normal'),
+                        input_shape=input_shape, name=conv_name_base + '2a')(input_tensor)
+    x = TimeDistributed(BatchNormalization(axis=3), name=bn_name_base + '2a')(x)
+    x = Activation('relu')(x)
+
+    x = TimeDistributed(Conv2D(nb_filter2, (kernel_size, kernel_size), padding='same', trainable=trainable,
+                               kernel_initializer='normal'), name=conv_name_base + '2b')(x)
+    x = TimeDistributed(BatchNormalization(axis=3), name=bn_name_base + '2b')(x)
+    x = Activation('relu')(x)
+
+    x = TimeDistributed(Conv2D(nb_filter3, (1, 1), kernel_initializer='normal'), name=conv_name_base + '2c',
+                        trainable=trainable)(x)
+    x = TimeDistributed(BatchNormalization(axis=3), name=bn_name_base + '2c')(x)
+
+    shortcut = TimeDistributed(
+        Conv2D(nb_filter3, (1, 1), strides=strides, trainable=trainable, kernel_initializer='normal'),
+        name=conv_name_base + '1')(input_tensor)
+    shortcut = TimeDistributed(BatchNormalization(axis=3), name=bn_name_base + '1')(shortcut)
+
+    x = Add()([x, shortcut])
+    x = Activation('relu')(x)
+    return x
+
+
+def classifier_layers(x, input_shape, trainable=False):
+    x = conv_block_td(x, 3, [512, 512, 2048], stage=5, block='a', input_shape=input_shape, strides=(2, 2),
+                      trainable=trainable)
+    x = identity_block_td(x, 3, [512, 512, 2048], stage=5, block='b', trainable=trainable)
+    x = identity_block_td(x, 3, [512, 512, 2048], stage=5, block='c', trainable=trainable)
+    x = TimeDistributed(AveragePooling2D((7, 7)), name='avg_pool')(x)
+
+    return x
+
+
+def get_classifier(base_layers, input_rois, num_rois, nb_classes=21, trainable=False):
+    pooling_regions = 14
+    input_shape = (num_rois, 14, 14, 1024)
+    # proposal 层相当于 [base_layers,input_rois]
+    out_roi_pool = RoiPoolingConv(pooling_regions, num_rois)([base_layers, input_rois])
+    # 以下是对于feature做操作
+    out = classifier_layers(out_roi_pool, input_shape=input_shape, trainable=True)
+    out = TimeDistributed(Flatten())(out)
+    # 输出bbox_pred
+    out_class = TimeDistributed(Dense(nb_classes, activation='softmax', kernel_initializer='zero'),
+                                name='dense_class_{}'.format(nb_classes))(out)
+    # 输出class_prob
+    out_regr = TimeDistributed(Dense(4 * (nb_classes - 1), activation='linear', kernel_initializer='zero'),
+                               name='dense_regress_{}'.format(nb_classes))(out)
+    return [out_class, out_regr]
+
+
+if __name__ == '__main__':
+    # roi_input 应该是输入的ROI
+    roi_input = Input(shape=(12996, 4))
+    # feature map input 应该是经过resnet后输出的图像
+    feature_map_input = Input(shape=(38, 38, 1024))
+    classifier = get_classifier(feature_map_input, roi_input, 32, nb_classes=11, trainable=True)
+    model_classifier_only = Model([feature_map_input, roi_input], classifier)
+    plot_model(model_classifier_only, to_file="classifier.png", show_shapes=True, show_layer_names=True)
+    model_classifier_only.summary()
