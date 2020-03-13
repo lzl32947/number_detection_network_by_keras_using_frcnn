7388f6e40a8cac4c2e3952b055fde05e2038d113
lzl32947@gmail.com <githublzl980223>
2020-03-13 13:52:16 +0800

    Change the parameter.py to configs.py.

---
+++ config/configs.py
@@ -0,0 +1,8 @@
+import colorsys
+
+
+class Config(object):
+    min_dim = 600
+    classifier_regr_std = [8.0, 8.0, 4.0, 4.0]
+    class_names = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    model_path = None
